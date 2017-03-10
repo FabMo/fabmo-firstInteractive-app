295164a0da3779810734982a27cf4c56a0817db6
tedh-shopbot <ted@shopbottools.com>
2017-03-10 11:26:17 -0500

    working except for positioning scaled path

--- js/makepath.js
+++ js/makepath.js
@@ -1,23 +1,31 @@
-        var fabmo = new FabMoDashboard();
-        var path;
+  // global position and tracking (to get in and out of paperScope)
+  window.globals = {
+    tool_x: 0,
+    tool_y: 0 
+    //someFunction: function() { alert(globals.someValue); }
+  }
 
-        var tool_x, tool_y;                      // global position and tracking
-        var last_pos_x = 0, last_pos_y = 0;
-        var pos, smooth_pt1, smooth_pt2;
+    var fabmo = new FabMoDashboard();        // seem to need a fabmo inside paperScope
+    var path;
+    var start_child = 5;                     // count for number of paths to remove on resize
+                                             // ... start at 5 a kludge to prevent premature erase
 
-        var err;
-        var m_rate;
-        var pt_ct = 0, seg_ct = 0, next_ct = 0;
-        var len_here = 0;
-        var smooth_pt = new Point();
+    var last_pos_x = 0, last_pos_y = 0;
+    var pos, smooth_pt1, smooth_pt2;
 
-        var tool_width = 6;                      // hard code size for handibot at moment
-        var tool_height = 8;
-        var tool_prop = tool_height / tool_width;
-        var min_margin = 10;
+    var err;
+    var m_rate;
+    var pt_ct = 0, seg_ct = 0, next_ct = 0;
+    var len_here = 0;
+    var smooth_pt = new Point();
 
-        var riScale = 1;                         // view scale for zoom
-        var riUnit = 45;                         // unit value pixels per
+    var tool_width = 6;                      // hard code size for handibot at moment
+    var tool_height = 8;
+    var tool_prop = tool_height / tool_width;
+    var min_margin = 10;
+
+    var riScale = 1;                         // view scale for zoom
+    var riUnit = 45;                         // unit value pixels per
 
         // - Set Starting View of Tool Work Area
         var fullview = new Rectangle();  
@@ -45,16 +53,14 @@
 
         // - Setup for tool Motion
         var circle = new Path.Circle(100,100, 10); //{seem to have to start with loc here}
-          circle.strokeColor = 'red';
+        circle.strokeColor = 'red';
 
         //view.center = circle.position;
 //        view.scale(riScale,-1 * riScale);
 
-//---------------------------Start Action
-        //onResize();
+//--------------------------- App Actions
 
-
-
+        var children = project.activeLayer.children;
         function onMouseDown(event) {
             // If we produced a path before, deselect it:
             if (path) {
@@ -67,6 +73,8 @@
                 // Select the path, so we can see its segment points:
                 fullySelected: true
             });
+        //children = project.activeLayer.children;
+        console.log("num children - ", children.length, start_child, children.length - 1);
         }
 
         // Dragging mouse adds points to path
@@ -121,52 +129,41 @@
 //===========================ACTION FUNCTIONS
 //---------------------------drawing
     function onResize () {
-//         defwrk.width = view.viewSize.width - (2 * min_margin);
       // - Update Bounding Box Representing Work Area and other values ...
       bheight = view.viewSize.height - (2 * min_margin);
       bwidth = view.viewSize.width - (2 * min_margin);
       defwrk = [min_margin, min_margin, bwidth, bheight];
-      bbox.fitBounds(defwrk);  // use rect defwrk to re-define scaling
+      bbox.fitBounds(defwrk);   // use rect defwrk to re-define scaling
       bbox.position = view.center;
-      riUnit = bbox.bounds.height / 8;
-//      circle.position.x = bbox.bounds.left + (tool_x * riUnit); 
-//      circle.position.y = bbox.bounds.bottom - (tool_y * riUnit); 
+      var startPos = new Point(circle.position);
+      var lastUnit = riUnit;
+      riUnit = bbox.bounds.height / tool_height;
+      var scaleUnit = riUnit/lastUnit;
+      var dX = bbox.bounds.left + (globals.tool_x * riUnit) - circle.position.x; 
+      var dY = bbox.bounds.bottom - (globals.tool_y * riUnit) - circle.position.y; 
+      circle.position.x += dX;
+      circle.position.y += dY; 
+      for (var i = start_child; i < (children.length - 1); i++) {
+        children[i].remove();
+      }
+      if (path){                // update size and loc of recent path
+        path.position.x += dX;
+        path.position.y += dY;
+        path.scale(scaleUnit); 
+    console.log("path:  ", path.position.x, path.position.y);
+      }
       textItem3.content = 'Screen: ' + view.viewSize.width.toFixed(1) + ', ' + view.viewSize.height.toFixed(1);
-      console.log("tool  " + status.posx + ", " + status.posy);
     }
-//---------------------------fabmo
-    // fabmo.on('status', function(status) {
-    //   tool_x = status.posx;
-    //   tool_y = status.posy;
-    //   circle.position.x = bbox.bounds.left + (tool_x * riUnit); 
-    //   circle.position.y = bbox.bounds.bottom - (tool_y * riUnit); 
-    //   textItem2.content = 'Tool Location: ' + tool_x.toFixed(3) + ', ' + tool_y.toFixed(3);
-    // });
-    // fabmo.getConfig(function(err, cfg) {
-    //   try {
-    //     if(cfg.machine.envelope) {
-    //       console.log(cfg.machine.envelope)
-    //       //display.setExtents(cfg.machine.envelope);
-    //       //display.gotoExtents(1000);
-    //     }              
-    //   } catch(e) {
-    //     console.error(e);
-    //   }
-    // });
-//--------------------------ready!
-    $(document).ready(function() {
-        onResize();   
-        fabmo.requestStatus(); // Make sure we have start location
-    //    textItem3.content = 'Screen: ' + riScope.viewSize.size;
-    console.log("style - ", paper.project.currentStyle.fillColor);
 
+//---------------------------fabmo
     fabmo.on('status', function(status) {
-      tool_x = status.posx;
-      tool_y = status.posy;
-      circle.position.x = bbox.bounds.left + (tool_x * riUnit); 
-      circle.position.y = bbox.bounds.bottom - (tool_y * riUnit); 
-      textItem2.content = 'Tool Location: ' + tool_x.toFixed(3) + ', ' + tool_y.toFixed(3);
+      globals.tool_x = status.posx;
+      globals.tool_y = status.posy;
+      circle.position.x = bbox.bounds.left + (globals.tool_x * riUnit); 
+      circle.position.y = bbox.bounds.bottom - (globals.tool_y * riUnit); 
+      textItem2.content = 'Tool Location: ' + globals.tool_x.toFixed(3) + ', ' + globals.tool_y.toFixed(3);
     });
+
     fabmo.getConfig(function(err, cfg) {
       try {
         if(cfg.machine.envelope) {
@@ -179,5 +176,10 @@
       }
     });
 
-
+//--------------------------ready!
+    $(document).ready(function() {
+        onResize();   
+        fabmo.requestStatus(); // Make sure we have start location
+//        start_child = project.activeLayer.children.length;
+    console.log("loaded:  ", paper.project, circle.position);
     });
